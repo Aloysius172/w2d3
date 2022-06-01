18b47e6a704520b18f185047714a746fb9f876d0
“Aloysius <aloysius.ekezie1@gmail.com>
2022-06-01 15:14:17 -0400

    finished battleship

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,4 +1,10 @@
 class Board
+    def self.print_grid(grid)
+        grid.each do |row|
+            puts row.join(" ")
+        end
+    end
+
     def initialize(n)
         @grid = Array.new(n) { Array.new(n, :N) }
         @size = (n * n)
@@ -26,13 +32,44 @@ class Board
         
         if self[position] == :S
             self[position] = :H
-            print "you sunk my battleship!"
+            puts "you sunk my battleship!"
             return true
         else
             self[position] = :X
             return false
         end
-
-
     end
+
+    def place_random_ships
+        ships = @size / 4
+        while  self.num_ships < ships
+            row = rand(0...@grid.length)
+            col = rand(0...@grid.length)
+            position = [row, col]
+            self[position] = :S
+        end
+    end
+
+    def hidden_ships_grid
+        @grid.map do |row|
+            row.map do |ele|
+                if ele == :S
+                      :N
+                else
+                      ele
+                end
+            end
+        end
+    end
+
+  
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+
+    def print
+        Board.print_grid(self.hidden_ships_grid)
+    end
+
 end
